c26c4da4442e67502b4d95770feaf45ffba56793
Jeroen Rosenberg <jrosenberg@xebia.com>
2019-07-08 21:39:28 +0200

    Added JMX exporter config to Kafka

--- apps/orders-consumer/src/main/scala/com/xebia/orders/OrderConsumer.scala
+++ apps/orders-consumer/src/main/scala/com/xebia/orders/OrderConsumer.scala
@@ -10,25 +10,5 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization._
 
 object OrderConsumer extends App {
-
-  private implicit val actorSystem = ActorSystem("orders")
-  implicit val mat: Materializer = ActorMaterializer()
-  implicit val ec = actorSystem.dispatcher
-
-  val settings = ConsumerSettings(actorSystem, new StringDeserializer, new ByteArrayDeserializer)
-    .withBootstrapServers("kafka:9092")
-    .withGroupId("my-group")
-    .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-
-  Consumer.plainSource(settings, Subscriptions.topics("orders"))
-    .map(_.value())
-    .map(new String(_))
-    .map(println)
-    .runWith(Sink.ignore).onComplete {
-      _ =>
-        println("Stream is dead!")
-        sys.exit(1)
-    }
-
-
+  // TODO: consume orders and do something useful with it
 }
